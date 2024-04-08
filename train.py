57ab1b61e7d33d5b665edcffe76fcdcda3a545c1
Chandni Ansari <74239667+cansari04@users.noreply.github.com>
2024-04-08 08:46:25 +0530

    train.py

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
 import pickle
 import numpy as np
 
@@ -10,10 +9,7 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-
-model = LogisticRegression(max_iter=1000, random_state=42).fit(X_scaled, y)
+model = LogisticRegression().fit(X, y
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
